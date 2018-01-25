8887eb1b9e68989eec71afe719ee1a74229a9ed8
jeet bafna <jeetanil97@gmail.com>
2018-01-25 19:12:45 +0530

    Verifying card with stripe

---
+++ public/javascripts/checkout.js
@@ -0,0 +1,30 @@
+Stripe.setPublishableKey('pk_test_EfA6Nz2ZlLml0wpQdW3oB2gf');
+ var $form = $('#checkout-form');
+ $form.submit(function(event){
+ 	$('#charge-error').addClass('hidden');
+ 	$form.find('button').prop('disabled', true);
+ 	Stripe.card.createToken({
+ 		number: $('#card-number').val(),
+ 		cvc: $('#card-cvc').val(),
+ 		exp_month: $('#card-expiry-month').val(),
+ 		exp_year: $('#card-expiry-year').val(),
+ 		name: $('#card-name').val()
+ 	}, stripeResponseHandler);
+ 	return false;
+ });
+
+function stripeResponseHandler(status, response){
+ 	if(response.error){
+ 		//Show errors on the form
+ 		$('#charge-error').text(response.error.message);
+ 		$('#charge-error').removeClass('hidden');
+ 		$form.find('button').prop('disabled', false); //Re-enable submission
+ 	} else{
+ 		//token was created
+ 		var token = response.id;
+ 		//Insert the token in the form
+ 		$form.append($('<input type="hidden" name="stripeToken" />').val(token));
+ 		//Submit the form
+ 		$form.get(0).submit();
+ 	}
+}
