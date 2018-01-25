89ee8906ef363e3456e3d34f220237f425fcc34c
jeet bafna <jeetanil97@gmail.com>
2018-01-25 19:55:45 +0530

    Charging the customer

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ var Product = require('../models/product');
 var Cart = require('../models/cart')
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	var successMsg = req.flash('success')[0];
 	Product.find(function(err, docs){
 		var productChunks = [];
 		var chunkSize = 3;
 		for(var i = 0; i< docs.length; i += chunkSize){
 			productChunks.push(docs.slice(i,i + chunkSize));
 		}
-		res.render('shop/index', { title: 'Shopping Cart', products: productChunks});
+		res.render('shop/index', { title: 'Shopping Cart', products: productChunks, successMsg: successMsg, noMessage: !successMsg});
 	});
   
 });
@@ -36,6 +37,7 @@ router.get('/shopping-cart', function(req, res, next){
 		return res.render('shop/shopping-cart', {products: null});
 	}
 	var cart = new Cart(req.session.cart);
+
 	res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
@@ -44,7 +46,37 @@ router.get('/checkout', function(req, res, next){
 		return res.redirect('shop/shopping-cart');
 	}
 	var cart = new Cart(req.session.cart);
-	res.render('shop/checkout', {total: cart.totalPrice});
+	var errMsg = req.flash('error')[0];
+	res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noErrors: !errMsg});
 });
 
+router.post('/checkout', function(req, res, next){
+	if(!req.session.cart){
+		return res.redirect('shop/shopping-cart');
+	}
+	var cart = new Cart(req.session.cart);
+
+	var stripe = require("stripe")(
+	  "sk_test_I6pbBfmq2WPWLMX7YTfM5xdf"
+	);
+
+	stripe.charges.create({
+	  amount: cart.totalPrice * 100,
+	  currency: "eur",
+	  source: req.body.stripeToken, // obtained with Stripe.js
+	  description: "Test Charge"
+	}, function(err, charge) {
+	  // asynchronously called
+	  if(err){
+	  	req.flash('error', err.message);
+	  	return res.redirect('/checkout');
+	  }
+
+	  req.flash('success','Successfully bought product!!');
+	  req.session.cart = null;
+	  res.redirect('/');
+	});
+
+
+});
 module.exports = router;
